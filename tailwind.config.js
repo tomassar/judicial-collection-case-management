87917c577501833f7a3bed639c857f02593736c6
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2024-05-21 13:06:43 -0400

    config: update tailwind config content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      'internal/views/**/*.templ',
+      'internal/templates/**/*.templ',
     ],
     darkMode: 'class',
     theme: {
